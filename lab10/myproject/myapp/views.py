cf369679bf7685e34b27e107b884963a7e83d66d
mrwave2019-wq <mrwave2019@gmail.com>
2026-03-23 14:27:59 +0700

    Add files via upload

---
+++ lab10/myproject/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from myapp.models import Person
+
+# Create your views here.
+def index(request):
+    all_Person = Person.objects.all()
+    return render(request,'index.html',{"all_person":all_Person})
+
+def about(request):
+     return render(request,'about.html')
+
+def form(request):
+    return render(request,'form.html')
